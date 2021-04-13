82765a5f89a01ab2cb9bfb3ba37fb83f6600d448
Sze Wai Yuen <szewaiyuen7@gmail.com>
2021-04-13 09:50:29 -0700

    added elastic search client and Gson

--- elasticsearch-consumer/src/main/java/ElasticSearchConsumer.java
+++ elasticsearch-consumer/src/main/java/ElasticSearchConsumer.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class ElasticSearchConsumer {
+
+import com.google.gson.Gson;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.util.List;
+import java.util.Properties;
+import com.google.gson.JsonParser;
+
+public class ElasticSearchConsumer {
+
+  private Gson gson;
+  public ElasticSearchConsumer() {
+    gson = new Gson();
+  }
+
+
+  private KafkaConsumer<String, String> createKafkaConsumer(String groupId, String offsetOption, List<String> topics) {
+    Properties properties = new Properties();
+    properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, Utils.BOOTSTRAP_SERVER);
+    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, offsetOption);
+    // Due to async nature, if auto commit is true, then we are under at most once behavior
+    properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+    // Batch request read from kafka
+    properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, Integer.toString(50));
+
+    // Create consumer
+    KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<String, String>(properties);
+    kafkaConsumer.subscribe(topics);
+    return kafkaConsumer;
+  }
+
+  private RestHighLevelClient createElasticSearchClient() {
+    CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+    credentialsProvider.setCredentials(AuthScope.ANY,
+            new UsernamePasswordCredentials(Utils.USERNAME, Utils.PASSWORD));
+
+    RestClientBuilder builder = RestClient.builder(
+            new HttpHost(Utils.HOSTNAME, 443, "https"))
+            .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+              @Override
+              public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+              }
+            });
+    return new RestHighLevelClient(builder);
+  }
 }
